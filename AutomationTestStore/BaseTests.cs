ed860fc796724bdea8c7b11fae139777184811a5
Lungile Morolong <lnombebe@gmail.com>
2025-11-04 19:46:30 +0200

    Create the BaseTest clase which includes the NUnit setup hook that runs before each test and NUnit teardown hook that runs after each test. Also include screenshot method.

---
+++ AutomationTestStore/BaseTests.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework;
+using Allure.Commons;
+
+namespace AutomationTestStore
+{
+    public class BaseTests
+    {
+        protected IWebDriver Driver;
+
+        [SetUp]
+
+        public void SetUp()
+        {
+            Driver = WebDriverFactory.CreateDriver();
+            Driver.Navigate().GoToUrl(Constants.BaseUrl);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+                TakeScreenshot();
+            }
+            Driver.Quit();
+        }
+
+
+        private void TakeScreenshot()
+        {
+            try
+            {
+                var screenshot = ((ITakesScreenshot)Driver).GetScreenshot();
+                string solutionDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", ".."));
+
+                string screenshotDir = Path.Combine(solutionDir, "Screenshots");
+
+                if (!Directory.Exists(screenshotDir))
+                {
+                    Directory.CreateDirectory(screenshotDir);
+                }
+
+                string screenshotFile = Path.Combine(screenshotDir, $"{TestContext.CurrentContext.Test.Name}_{DateTime.Now:yyyyMMdd_HHmmss}.png");
+                screenshot.SaveAsFile(screenshotFile);
+
+                AllureLifecycle.Instance.AddAttachment("Screenshot on failure", "image/png", screenshotFile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to take screenshot: {ex.Message}");
+            }
+        }
+    }
+}
