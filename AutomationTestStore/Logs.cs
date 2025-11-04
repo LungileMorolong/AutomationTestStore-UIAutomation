efe40483b7189eacf6af0258573afac4dd2072a0
Lungile Morolong <lnombebe@gmail.com>
2025-11-04 19:48:19 +0200

    Created Logs class which uses console output and Allure test reporting for NUnit automated tests.

---
+++ AutomationTestStore/Logs.cs
@@ -0,0 +1,29 @@
+﻿using Allure.Commons;
+using NUnit.Allure.Core;
+
+namespace AutomationTestStore
+{
+    public static class Logs
+    {
+        static Logs()
+        {
+            AllureLifecycle.Instance.CleanupResultDirectory();  
+        }
+
+        public static void Step(string message)
+        {
+            Console.WriteLine($"Step: {message}");
+            AllureLifecycle.Instance.WrapInStep(delegate
+            {
+            }, "Step: " + message, "Step");
+        }
+
+        public static void Info(string message)
+        {
+            Console.WriteLine($"Info: {message}");
+            AllureLifecycle.Instance.WrapInStep(delegate
+            {
+            }, "Info: " + message, "Info");
+        }
+    }
+}
