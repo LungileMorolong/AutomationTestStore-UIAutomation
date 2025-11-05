d2fb944778c0edb4eacfe0b569173586009f026f
Lungile Morolong <lnombebe@gmail.com>
2025-11-05 23:44:58 +0200

    Created the Login and Logout Tests includes NUnit Test fixtures and assertions. The Logout tests has a positive and two negative tests.

---
+++ AutomationTestStore/Tests/LogOutTest.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Allure.Attributes;
+using Allure.Commons;
+using NUnit.Framework;
+using NUnit.Allure.Core;
+using AutomationTestStore.Pages;
+
+namespace AutomationTestStore.Tests
+{
+    [TestFixture]
+    [AllureNUnit]
+    [AllureSuite("Logout Test")]
+    public class LogOutTest : BaseTests
+    {
+        [Test(Description = "User logout from account")]
+        [AllureTag("Logout", "Positive")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureOwner("QA Team")]
+        public void UserLogoutFromAccount()
+        {
+            HomePage homePage = new HomePage(Driver);
+            AccountLoginPage accountLoginPage = new AccountLoginPage(Driver);
+            MyAccountPage myAccountPage = new MyAccountPage(Driver);
+            LogoutPage logoutPage = new LogoutPage(Driver);
+
+            homePage.ClickOnLoginOrRegister();
+            accountLoginPage.EnterLoginDetails("PlayoinTeam", "SecurePassword123");
+            accountLoginPage.ClickOnLoginButton();
+            myAccountPage.ClickOnLogoutLinkText();
+
+            var logoutMessage = logoutPage.GetLogoutMessage();
+            try
+            {
+                Assert.That(logoutMessage, Does.Contain("You have been logged off your account. It is now safe to leave the computer."));
+                Logs.Info("User logged out successfully and verified.");
+            }
+            catch (AssertionException ex)
+            {
+                Logs.Info($"Assertion failed: {ex.Message}");
+                throw;
+            }
+
+            logoutPage.ClickOnContinueButton();
+
+            var homeLoginOrRegistationText = homePage.GetLoginOrRegisterText();
+
+            try
+            {
+                Assert.That(homeLoginOrRegistationText, Does.Contain("Login or register"));
+                Logs.Info("User is navigated back to home page after logout and verified.");
+            }
+            catch (AssertionException ex)
+            {
+                Logs.Info($"Assertion failed: {ex.Message}");
+                throw;
+            }
+
+
+        }
+    }
+}
---
+++ AutomationTestStore/Tests/LoginTest.cs
@@ -0,0 +1,95 @@
+﻿using NUnit.Allure.Attributes;
+using Allure.Commons;
+using NUnit.Framework;
+using NUnit.Allure.Core;
+using AutomationTestStore.Pages;
+
+namespace AutomationTestStore.Tests
+{
+    [TestFixture]
+    [AllureNUnit]
+    [AllureSuite("User Login")]
+    public class LoginTest : BaseTests
+    {
+        [Test(Description = "User login into account")]
+        [AllureTag("Login", "Positive")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureOwner("QA Team")]
+        public void UserLoginIntoAccount()
+        {
+            HomePage homePage = new HomePage(Driver);
+            AccountLoginPage accountLoginPage = new AccountLoginPage(Driver);
+            MyAccountPage myAccountPage = new MyAccountPage(Driver);
+
+            homePage.ClickOnLoginOrRegister();
+            accountLoginPage.EnterLoginDetails("PlayoinTeam", "SecurePassword123");
+            accountLoginPage.ClickOnLoginButton();
+
+            var welcomeText = myAccountPage.GetWelcomeBackText();
+            try
+            {
+                Assert.That(welcomeText, Does.Contain($"Welcome back {"Playi"}"));
+                Logs.Info("User logged in successfully and verified.");
+            }
+            catch (AssertionException ex)
+            {
+                Logs.Info($"Assertion failed: {ex.Message}");
+                throw;
+            }
+        }
+
+        [Test(Description = "User login using invalid username")]
+        [AllureTag("Login", "Negative")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureOwner("QA Team")]
+        public void UserLoginWithInvalidUserName()
+        {
+            HomePage homePage = new HomePage(Driver);
+            AccountLoginPage accountLoginPage = new AccountLoginPage(Driver);
+            MyAccountPage myAccountPage = new MyAccountPage(Driver);
+
+            homePage.ClickOnLoginOrRegister();
+            accountLoginPage.EnterLoginDetails("PlayonTeam", "SecurePassword123");
+            accountLoginPage.ClickOnLoginButton();
+
+            var errorMessage = accountLoginPage.GetInvalidLoginWarningMessage();
+            try
+            {
+                Assert.That(errorMessage, Does.Contain("Error: Incorrect login or password provided."));
+                Logs.Info("The correct error message appear indicating invalid username or password was provided");
+            }
+            catch (AssertionException ex)
+            {
+                Logs.Info($"Assertion failed: {ex.Message}");
+                throw;
+            }
+        }
+
+        [Test(Description = "User login using invalid password")]
+        [AllureTag("Login", "Negative")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureOwner("QA Team")]
+        public void UserLoginWithInvalidPassword()
+        {
+            HomePage homePage = new HomePage(Driver);
+            AccountLoginPage accountLoginPage = new AccountLoginPage(Driver);
+            MyAccountPage myAccountPage = new MyAccountPage(Driver);
+
+            homePage.ClickOnLoginOrRegister();
+            accountLoginPage.EnterLoginDetails("PlayoinTeam", "SecurePassword13");
+            accountLoginPage.ClickOnLoginButton();
+
+            var errorMessage = accountLoginPage.GetInvalidLoginWarningMessage();
+            try
+            {
+                Assert.That(errorMessage, Does.Contain("Error: Incorrect login or password provided."));
+                Logs.Info("The correct error message appear indicating invalid username or password was provided");
+            }
+            catch (AssertionException ex)
+            {
+                Logs.Info($"Assertion failed: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
