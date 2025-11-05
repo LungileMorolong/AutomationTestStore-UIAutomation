df127100794d58401b111f8c9eda01e7f8a48bb6
Lungile Morolong <lnombebe@gmail.com>
2025-11-05 22:35:23 +0200

    Added remove button element and methods as well as for empty cart message

--- AutomationTestStore/Pages/ShoppingCartPage.cs
+++ AutomationTestStore/Pages/ShoppingCartPage.cs
@@ -7,6 +7,8 @@ namespace AutomationTestStore.Pages
         #region Private Fields
         private By productName = By.CssSelector("#cart > div > div.container-fluid.cart-info.product-list > table > tbody > tr:nth-child(2) > td:nth-child(2) > a");
         private By quantityNumber = By.CssSelector("#cart_quantity123d4ba17727b9155709742d2033b6e8e11");
+        private By removeButton = By.CssSelector("#cart > div > div.container-fluid.cart-info.product-list > table > tbody > tr:nth-child(2) > td:nth-child(7) > a");
+        private By emptyCartMessage = By.CssSelector("#maincontainer > div > div > div > div");
         #endregion
 
         #region Constructor
@@ -26,6 +28,16 @@ namespace AutomationTestStore.Pages
             Logs.Step("Getting Quantity Number");
             string quantityText = GetAttribute(quantityNumber, "value");
             return int.Parse(quantityText);
+        }
+        public void ClickOnRemoveButton()
+        {
+            Logs.Step("Clicking on Remove Button");
+            Click(removeButton);
+        }
+        public string GetEmptyCartMessage()
+        {
+            Logs.Step("Getting Empty Cart Message");
+            return GetText(emptyCartMessage);
         }
         #endregion
 
