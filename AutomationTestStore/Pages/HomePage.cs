02e04bd7f55416bdb714c9f920deb5707a7916fa
Lungile Morolong <lnombebe@gmail.com>
2025-11-05 11:58:08 +0200

    Created the Home Page class which encapsulates the page locators and public methods

---
+++ AutomationTestStore/Pages/HomePage.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+
+namespace AutomationTestStore.Pages
+{
+    public class HomePage : PageObjects
+    {
+        #region Private Fields
+        private By homeMenuOption = By.CssSelector("#categorymenu > nav > ul > li:nth-child(1) > a");
+        private By apprealAndAccessoriesMenuOption = By.CssSelector("#categorymenu > nav > ul > li:nth-child(2) > a");
+        private By tshirtDropDownOption = By.CssSelector("#categorymenu > nav > ul > li:nth-child(2) > div > ul:nth-child(1) > li:nth-child(2) > a");
+        private By loginOrRegister = By.CssSelector("#customer_menu_top > li > a");
+        #endregion
+
+        #region Constructor
+        public HomePage(IWebDriver driver) : base(driver)
+        {
+        }
+        #endregion
+
+        #region Public Methods
+        public void ClickOnHomeMenuOption()
+        {
+            Logs.Step("Clicking on Home Menu Option");
+            Click(homeMenuOption);
+        }
+
+        public void SelectTShirtFromApprealAndAccessoriesMenu()
+        {
+            Logs.Step("Selecting T-Shirt from Appreal and Accessories Menu");
+            HoverAndSelectDropdown(apprealAndAccessoriesMenuOption, tshirtDropDownOption);
+        }
+
+        public void ClickOnLoginOrRegister()
+        {
+            Logs.Step("Clicking on Login or Register");
+            Click(loginOrRegister);
+        }
+        #endregion
+    }
+}
